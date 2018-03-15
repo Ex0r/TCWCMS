984c21ce7dac8993016fc29619eb649d03b4ae5d
Justin Furnas <jfurnas@cenmi.org>
2018-03-15 14:36:47 -0400

    Made the 'active' links on the sidebar dynamic based on the current path you are on

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,31 +2,31 @@
   <div class="sidebar-sticky">
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link active" href="/admin/">
+        <a class="nav-link @if (\Request::is('admin')) active @endif" href="/admin/">
           <span data-feather="home"></span>
           Dashboard <span class="sr-only">(current)</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/admin/accounts/">
+        <a class="nav-link @if (Request::is('admin/accounts','admin/accounts/*')) active @endif" href="/admin/accounts/">
           <span data-feather="users"></span>
           Accounts
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/admin/pages/">
+        <a class="nav-link @if (\Request::is('admin/pages','admin/pages/*')) active @endif" href="/admin/pages/">
           <span data-feather="file"></span>
           Pages
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/admin/support/">
+        <a class="nav-link @if (\Request::is('admin/support','admin/support/*')) active @endif" href="/admin/support/">
           <span data-feather="help-circle"></span>
           Support Tickets
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/admin/settings/">
+        <a class="nav-link @if (\Request::is('admin/settings','admin/settings/*')) active @endif" href="/admin/settings/">
           <span data-feather="settings"></span>
           Settings
         </a>
